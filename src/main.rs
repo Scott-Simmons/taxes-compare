8238f8a09dabe5611442ede6e1cce2d2afca9cf3
Scott-Simmons <52365471+Scott-Simmons@users.noreply.github.com>
2024-09-14 16:04:10 +1000

    Test for getting breakeven points

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use log::debug;
+use itertools::Itertools;
 use nalgebra;
 use std::{cmp::Ordering, collections::HashMap};
 // list of (r, b)
@@ -24,6 +24,7 @@ struct MarginalRateKnot {
 }
 
 /// A point characterised by tax amount at given income, which is also denoted as a knot point
+#[derive(Clone, Debug)]
 struct IncomeTaxKnot {
     /// Income tax amount f(x) for a given maximimum income level x
     income_tax_amount: f32,
@@ -32,7 +33,7 @@ struct IncomeTaxKnot {
 }
 
 /// A point characterised by tax amount at a given income
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 struct IncomeTaxPoint {
     /// Income tax amount f(x) for given level of income x
     income_tax_amount: f32,
@@ -86,22 +87,60 @@ impl IncomeTaxSchedule {
     }
 }
 
+
 fn compute_breakeven_points(
-    segment_1: Vec<LinearPiecewiseSegment>,
-    segment_2: Vec<LinearPiecewiseSegment>,
-) -> IncomeTaxPoint {
-    IncomeTaxPoint {
-        income_tax_amount: 0.0,
-        income: 0.0,
+    knots1: Vec<IncomeTaxKnot>,
+    knots2: Vec<IncomeTaxKnot>,
+) -> Vec<IncomeTaxPoint> {
+    // Do not forget that for knots derived from tax schedules we need to define an upper bound (cannot be inf, inf...)
+
+    let mut i = 0; // curve 1
+    let mut j = 0; // curve 2
+    let mut breakeven_points = Vec::new();
+
+    while i + 1 < knots1.len() && j + 1 < knots2.len() {
+        let l1 = knots1[i].income_limit;
+        let r1 = knots1[i + 1].income_limit;
+        let l2 = knots2[j].income_limit;
+        let r2 = knots2[j + 1].income_limit;
+
+        let segments_have_overlap = r1 >= l1 && r2 >= l1;
+
+        if segments_have_overlap {
+            let candidate_segment = LinearPiecewiseSegment {
+                left_point: knots2[j].clone(),
+                right_point: knots2[j + 1].clone(),
+            };
+            let intersection = LinearPiecewiseSegment {
+                left_point: knots1[i].clone(),
+                right_point: knots1[i + 1].clone(),
+            }
+            .compute_intersection(&candidate_segment);
+            match intersection {
+                None => {} // do nothing
+                Some(breakeven_point) => {
+                    breakeven_points.push(breakeven_point); // add to result set
+                }
+            };
+        }
+
+        if r1 < r2 {
+            i += 1; // advance 1
+        } else {
+            j += 1; // advance 2
+        }
     }
+    breakeven_points
 }
 
 /// A line segment characterised by two points
+#[derive(Clone, Debug)]
 struct LinearPiecewiseSegment {
     /// Two knot points characterise a segment of a linear piecewise function
     /// https://en.wikipedia.org/wiki/Line_segment
     left_point: IncomeTaxKnot,
     right_point: IncomeTaxKnot,
+    // some kind of assert so that left point x value always less than right point x value
 }
 
 /// Linearly interpolate a line segment at an income value, to get a taxation value.
@@ -123,7 +162,7 @@ impl LinearPiecewiseSegment {
     /// points in R^2 and t \in [0, 1]
     /// Then equate l1 = l2 and solve under those constraints
     /// The maths yields t1(b1 - a1) + t2(a2 - b2) = (a2 - a1)
-    /// This can then be bought into the form At = c
+    /// This can then be brought into the form At = c
     /// Three types of solution: Independent (one solution), Dependent (infininity many -
     /// return the whole line, inconsistent - return None). Rank(A) = 2, Rank(A) < 2, Rank(A) >
     /// 2
@@ -162,11 +201,11 @@ impl LinearPiecewiseSegment {
                         let (t1, t2) = (solution[(0, 0)], solution[(1, 0)]);
                         if (0.0 <= t1 && t1 <= 1.0) && (0.0 <= t2 && t2 <= 1.0) {
                             Some(IncomeTaxPoint {
-                                income_tax_amount: x1 + t1 * (x2 - x1),
-                                income: y1 + t1 * (y2 - y1),
+                                income_tax_amount: y1 + t1 * (y2 - y1),
+                                income: x1 + t1 * (x2 - x1),
                             })
                         } else {
-                            None
+                            None // No intersection within the segment range.
                         }
                     }
                 }
@@ -176,6 +215,7 @@ impl LinearPiecewiseSegment {
 }
 
 /// TODO: This might not be needed becuase the curve can be parameterised by the income knots.
+/// Will still be needed
 fn compute_income_taxes(
     income_start: f32,
     income_stop: f32,
@@ -311,7 +351,6 @@ mod tests {
             }
         );
 
-
         let non_interecting_segment = LinearPiecewiseSegment {
             left_point: IncomeTaxKnot {
                 income_limit: 4.0,
@@ -328,7 +367,6 @@ mod tests {
         let result = test_segment.compute_intersection(&test_segment);
         assert!(result.is_none());
 
-
         let parallel_segment = LinearPiecewiseSegment {
             left_point: IncomeTaxKnot {
                 income_limit: 0.0,
@@ -341,6 +379,82 @@ mod tests {
         };
         let result = parallel_segment.compute_intersection(&test_segment);
         assert!(result.is_none());
+    }
+
+    fn income_points_are_approx_eq(
+        point1: IncomeTaxPoint,
+        point2: IncomeTaxPoint,
+        tol: f32,
+    ) -> bool {
+        let x1 = point1.income;
+        let x2 = point2.income;
+        let y1 = point1.income_tax_amount;
+        let y2 = point2.income_tax_amount;
+        ((x2 - x1).powi(2) + (y2 - y1).powi(2)).sqrt() < tol
+    }
+
+    #[test]
+    fn test_get_breakeven_points() {
+        use crate::compute_breakeven_points;
+        use assert_approx_eq::assert_approx_eq;
+
+        // https://www.desmos.com/calculator
+        // Breakevens at (25/3, 20/3) and (50/3, 40/3)
+
+        let curve1 = vec![
+            IncomeTaxKnot {
+                income_limit: 0.0,
+                income_tax_amount: 0.0,
+            },
+            IncomeTaxKnot {
+                income_limit: 5.0,
+                income_tax_amount: 5.0,
+            },
+            IncomeTaxKnot {
+                income_limit: 15.0,
+                income_tax_amount: 10.0,
+            },
+            IncomeTaxKnot {
+                income_limit: 20.0,
+                income_tax_amount: 20.0,
+            },
+        ];
+        let curve2 = vec![
+            IncomeTaxKnot {
+                income_limit: 5.0,
+                income_tax_amount: 0.0,
+            },
+            IncomeTaxKnot {
+                income_limit: 10.0,
+                income_tax_amount: 10.0,
+            },
+            IncomeTaxKnot {
+                income_limit: 20.0,
+                income_tax_amount: 15.0,
+            },
+        ];
+
+        let breakevens = compute_breakeven_points(curve1, curve2);
+        let tolerance = 1e-5;
+        assert!(income_points_are_approx_eq(
+            breakevens[0].clone(),
+            IncomeTaxPoint {
+                income: 25.0 / 3.0,
+                income_tax_amount: 20.0 / 3.0
+            },
+            tolerance
+        ));
+
+        assert!(income_points_are_approx_eq(
+            breakevens[1].clone(),
+            IncomeTaxPoint {
+                income: 50.0 / 3.0,
+                income_tax_amount: 40.0 / 3.0
+            },
+            tolerance
+        ));
+
+        print!("{:?}", breakevens)
     }
 }
 
