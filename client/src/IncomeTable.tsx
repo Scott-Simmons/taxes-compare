16f948b3794125952d25e3100c37aacdfdf6f8e9
Scott-Simmons <52365471+Scott-Simmons@users.noreply.github.com>
2024-10-08 00:42:30 +1100

    Fix rates rendering
    Fix 'off by 100x' error on the frontend

--- client/src/IncomeTable.tsx
+++ client/src/IncomeTable.tsx
@@ -32,7 +32,7 @@ const IncomeData: React.FC<IncomeDataProps> = ({ data, income, currency }) => {
 
   const dataArray = Object.entries(data.country_specific_data).map(([country, taxInfo]) => ({ country, taxInfo }));
 
-  // Null handling needed to keep ts happy even though null income implies null tax amounts.
+  // Null handling needed to keep TS happy even though null income implies null tax amounts.
   dataArray.sort((a,b) => (a.taxInfo.specific_tax_amount || 0) - (b.taxInfo.specific_tax_amount || 0));
 
   const rankedDataArray = dataArray.map((entry, index) => ({ ...entry, rank: index + 1 }));
@@ -42,7 +42,7 @@ const IncomeData: React.FC<IncomeDataProps> = ({ data, income, currency }) => {
       <td>{entry.country}</td>
       <td>{entry.rank}</td>
       {/*Never will be null if income is null, but need these checks to keep TS happy*/}
-      <td>{entry.taxInfo.specific_tax_rate?.toFixed(2)}</td>
+      <td>{((entry.taxInfo.specific_tax_rate || 0.0) * 100.0).toFixed(2)}</td>
       <td>{entry.taxInfo.specific_tax_amount?.toFixed(0)}</td>
       <td>{(income - (entry.taxInfo.specific_tax_amount || 0.0)).toFixed(0)}</td>
     </tr>
